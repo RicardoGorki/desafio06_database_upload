d42c691916a0ffebff5e225d0949b05a2f7fc7ac
Ricardo Gorki <ricardo.gorki@gmail.com>
2021-01-06 23:20:45 -0300

    model migrations transactions route

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,29 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (sumTotal: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            sumTotal.income += transaction.value;
+            break;
+          case 'outcome':
+            sumTotal.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return sumTotal;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,34 @@
 // import AppError from '../errors/AppError';
-
+import { getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface RequestDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: RequestDTO): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+      category,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
